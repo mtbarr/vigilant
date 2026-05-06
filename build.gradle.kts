a3ed58d84044cb4b3faa44d6ad163361a090ca61
mtbarr <mtbarrdev@gmail.com>
2026-05-06 18:37:32 -0300

    chore(docker-compose): migrate SOCK_PATH to Quarkus HTTP domain socket vars

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,6 @@ dependencies {
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-rest-jackson")
     implementation("io.quarkus:quarkus-arc")
-    implementation("io.quarkus:quarkus-netty")
-    implementation("io.netty:netty-transport-native-epoll") {
-        artifact { classifier = "linux-x86_64" }
-    }
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
 }
